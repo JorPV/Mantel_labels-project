4a7a53afcfea768788965a73c03b29deafd9374d
JordiPV <88150345+JorPV@users.noreply.github.com>
2023-03-08 15:42:31 +0100

    Create javaScript.js

---
+++ javaScript.js
@@ -0,0 +1,32 @@
+// Check if the label contains a QR
+	let labelHasQR = true;
+	const labelsSelect = document.querySelector('#product-label-layout-id-batch');	
+	const inputBtn = document.querySelector('#submit-button-product-label');
+	const printBtnDisabled = document.querySelector('#button-disabled');
+	const printBtnActive = document.querySelector('#print-button');
+
+	labelsSelect.addEventListener('change', () => {
+		if(labelsSelect.options[labelsSelect.selectedIndex].dataset.containsQr == 1) {
+			labelHasQR = true;
+		} else {
+			labelHasQR = false;
+		};
+		return labelHasQR
+	});
+
+	// Check if there is a product without a URL in the form table.
+	let productWithoutUrl = false; 
+
+	function checkTableRows (){
+		const productLabelForm = document.querySelector('tbody');
+		let rows = productLabelForm.querySelectorAll('tr');
+		rows = Array.from(rows);
+		rows.forEach(product => {
+			const warningIcon = document.querySelector('.fa-exclamation-circle');
+			if(labelHasQR == true){
+				warningIcon.style.visibility = 'visible';
+			} else {
+				warningIcon.style.visibility = 'hidden';
+			}
+    });
+  }
